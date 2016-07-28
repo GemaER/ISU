59de1506e8189c2fee0151416b28994f1c216e14
GemaClops <giu.maa@hotmail.com>
2016-07-27 22:43:52 -0300

    Add files via upload

---
+++ ISU PROG/Entrega2/ISU/Ordeñe.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmOrdeñe
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlBlue = New System.Windows.Forms.Panel()
+        Me.gbxLeche = New System.Windows.Forms.GroupBox()
+        Me.tbxGrasa = New System.Windows.Forms.TextBox()
+        Me.lblGrasa = New System.Windows.Forms.Label()
+        Me.tbxProteina = New System.Windows.Forms.TextBox()
+        Me.lblProteina = New System.Windows.Forms.Label()
+        Me.tbxUrea = New System.Windows.Forms.TextBox()
+        Me.lblUrea = New System.Windows.Forms.Label()
+        Me.tbxRecuentocel = New System.Windows.Forms.TextBox()
+        Me.lblRecuentoCel = New System.Windows.Forms.Label()
+        Me.tbxRecuentobac = New System.Windows.Forms.TextBox()
+        Me.lblLitros = New System.Windows.Forms.Label()
+        Me.lblRecuentobac = New System.Windows.Forms.Label()
+        Me.tbxLitros = New System.Windows.Forms.TextBox()
+        Me.gbxLeche.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlBlue
+        '
+        Me.pnlBlue.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.pnlBlue.Location = New System.Drawing.Point(803, 0)
+        Me.pnlBlue.Name = "pnlBlue"
+        Me.pnlBlue.Size = New System.Drawing.Size(24, 579)
+        Me.pnlBlue.TabIndex = 72
+        Me.pnlBlue.Visible = False
+        '
+        'gbxLeche
+        '
+        Me.gbxLeche.Controls.Add(Me.tbxGrasa)
+        Me.gbxLeche.Controls.Add(Me.lblGrasa)
+        Me.gbxLeche.Controls.Add(Me.tbxProteina)
+        Me.gbxLeche.Controls.Add(Me.lblProteina)
+        Me.gbxLeche.Controls.Add(Me.tbxUrea)
+        Me.gbxLeche.Controls.Add(Me.lblUrea)
+        Me.gbxLeche.Controls.Add(Me.tbxRecuentocel)
+        Me.gbxLeche.Controls.Add(Me.lblRecuentoCel)
+        Me.gbxLeche.Controls.Add(Me.tbxRecuentobac)
+        Me.gbxLeche.Controls.Add(Me.lblLitros)
+        Me.gbxLeche.Controls.Add(Me.lblRecuentobac)
+        Me.gbxLeche.Controls.Add(Me.tbxLitros)
+        Me.gbxLeche.Enabled = False
+        Me.gbxLeche.Location = New System.Drawing.Point(38, 45)
+        Me.gbxLeche.Name = "gbxLeche"
+        Me.gbxLeche.Size = New System.Drawing.Size(399, 282)
+        Me.gbxLeche.TabIndex = 73
+        Me.gbxLeche.TabStop = False
+        Me.gbxLeche.Text = "Lactancia"
+        '
+        'tbxGrasa
+        '
+        Me.tbxGrasa.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxGrasa.Location = New System.Drawing.Point(251, 57)
+        Me.tbxGrasa.MaxLength = 3
+        Me.tbxGrasa.Name = "tbxGrasa"
+        Me.tbxGrasa.Size = New System.Drawing.Size(142, 32)
+        Me.tbxGrasa.TabIndex = 57
+        Me.tbxGrasa.Text = "000"
+        '
+        'lblGrasa
+        '
+        Me.lblGrasa.AutoSize = True
+        Me.lblGrasa.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGrasa.Location = New System.Drawing.Point(6, 60)
+        Me.lblGrasa.Name = "lblGrasa"
+        Me.lblGrasa.Size = New System.Drawing.Size(199, 24)
+        Me.lblGrasa.TabIndex = 56
+        Me.lblGrasa.Text = "Porcentaje de grasa"
+        '
+        'tbxProteina
+        '
+        Me.tbxProteina.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxProteina.Location = New System.Drawing.Point(251, 100)
+        Me.tbxProteina.MaxLength = 3
+        Me.tbxProteina.Name = "tbxProteina"
+        Me.tbxProteina.Size = New System.Drawing.Size(142, 32)
+        Me.tbxProteina.TabIndex = 55
+        Me.tbxProteina.Text = "000"
+        '
+        'lblProteina
+        '
+        Me.lblProteina.AutoSize = True
+        Me.lblProteina.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProteina.Location = New System.Drawing.Point(6, 103)
+        Me.lblProteina.Name = "lblProteina"
+        Me.lblProteina.Size = New System.Drawing.Size(222, 24)
+        Me.lblProteina.TabIndex = 54
+        Me.lblProteina.Text = "Porcentaje de proteina"
+        '
+        'tbxUrea
+        '
+        Me.tbxUrea.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxUrea.Location = New System.Drawing.Point(251, 147)
+        Me.tbxUrea.MaxLength = 3
+        Me.tbxUrea.Name = "tbxUrea"
+        Me.tbxUrea.Size = New System.Drawing.Size(142, 32)
+        Me.tbxUrea.TabIndex = 53
+        Me.tbxUrea.Text = "000"
+        '
+        'lblUrea
+        '
+        Me.lblUrea.AutoSize = True
+        Me.lblUrea.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUrea.Location = New System.Drawing.Point(6, 150)
+        Me.lblUrea.Name = "lblUrea"
+        Me.lblUrea.Size = New System.Drawing.Size(188, 24)
+        Me.lblUrea.TabIndex = 52
+        Me.lblUrea.Text = "Porcentaje de urea"
+        '
+        'tbxRecuentocel
+        '
+        Me.tbxRecuentocel.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxRecuentocel.Location = New System.Drawing.Point(251, 224)
+        Me.tbxRecuentocel.MaxLength = 11
+        Me.tbxRecuentocel.Name = "tbxRecuentocel"
+        Me.tbxRecuentocel.Size = New System.Drawing.Size(142, 32)
+        Me.tbxRecuentocel.TabIndex = 51
+        Me.tbxRecuentocel.Text = "00000000000"
+        '
+        'lblRecuentoCel
+        '
+        Me.lblRecuentoCel.AutoSize = True
+        Me.lblRecuentoCel.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRecuentoCel.Location = New System.Drawing.Point(6, 224)
+        Me.lblRecuentoCel.Name = "lblRecuentoCel"
+        Me.lblRecuentoCel.Size = New System.Drawing.Size(200, 48)
+        Me.lblRecuentoCel.TabIndex = 50
+        Me.lblRecuentoCel.Text = "Recuento de células" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "      somáticas"
+        '
+        'tbxRecuentobac
+        '
+        Me.tbxRecuentobac.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxRecuentobac.Location = New System.Drawing.Point(251, 185)
+        Me.tbxRecuentobac.MaxLength = 11
+        Me.tbxRecuentobac.Name = "tbxRecuentobac"
+        Me.tbxRecuentobac.Size = New System.Drawing.Size(142, 32)
+        Me.tbxRecuentobac.TabIndex = 49
+        Me.tbxRecuentobac.Text = "00000000000"
+        '
+        'lblLitros
+        '
+        Me.lblLitros.AutoSize = True
+        Me.lblLitros.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLitros.Location = New System.Drawing.Point(6, 22)
+        Me.lblLitros.Name = "lblLitros"
+        Me.lblLitros.Size = New System.Drawing.Size(63, 24)
+        Me.lblLitros.TabIndex = 46
+        Me.lblLitros.Text = "Litros"
+        '
+        'lblRecuentobac
+        '
+        Me.lblRecuentobac.AutoSize = True
+        Me.lblRecuentobac.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRecuentobac.Location = New System.Drawing.Point(6, 188)
+        Me.lblRecuentobac.Name = "lblRecuentobac"
+        Me.lblRecuentobac.Size = New System.Drawing.Size(205, 24)
+        Me.lblRecuentobac.TabIndex = 48
+        Me.lblRecuentobac.Text = "Recuento bacteriano"
+        '
+        'tbxLitros
+        '
+        Me.tbxLitros.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxLitros.Location = New System.Drawing.Point(251, 19)
+        Me.tbxLitros.MaxLength = 11
+        Me.tbxLitros.Name = "tbxLitros"
+        Me.tbxLitros.Size = New System.Drawing.Size(142, 32)
+        Me.tbxLitros.TabIndex = 47
+        Me.tbxLitros.Text = "00000000000"
+        '
+        'frmOrdeñe
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(832, 536)
+        Me.Controls.Add(Me.gbxLeche)
+        Me.Controls.Add(Me.pnlBlue)
+        Me.Name = "frmOrdeñe"
+        Me.Text = "frmOrdeñe"
+        Me.gbxLeche.ResumeLayout(False)
+        Me.gbxLeche.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pnlBlue As System.Windows.Forms.Panel
+    Friend WithEvents gbxLeche As System.Windows.Forms.GroupBox
+    Friend WithEvents tbxGrasa As System.Windows.Forms.TextBox
+    Friend WithEvents lblGrasa As System.Windows.Forms.Label
+    Friend WithEvents tbxProteina As System.Windows.Forms.TextBox
+    Friend WithEvents lblProteina As System.Windows.Forms.Label
+    Friend WithEvents tbxUrea As System.Windows.Forms.TextBox
+    Friend WithEvents lblUrea As System.Windows.Forms.Label
+    Friend WithEvents tbxRecuentocel As System.Windows.Forms.TextBox
+    Friend WithEvents lblRecuentoCel As System.Windows.Forms.Label
+    Friend WithEvents tbxRecuentobac As System.Windows.Forms.TextBox
+    Friend WithEvents lblLitros As System.Windows.Forms.Label
+    Friend WithEvents lblRecuentobac As System.Windows.Forms.Label
+    Friend WithEvents tbxLitros As System.Windows.Forms.TextBox
+End Class
---
+++ ISU PROG/Entrega2/ISU/Publico.vb
@@ -0,0 +1,209 @@
+﻿Module Publico
+    Public idioma, opcion As Integer
+    Public anestro, toro, novillo, vaca, vaquillona, macho, hembra, ternera, ternero, gen, buscar, ninguno As String
+    Public servicio, preñada, lactancia, seca, campo, rodeo, tambo, raza, division, lugar, etapa, sexo, hembraa As String
+    Public contraseña, usuario, iniciar, ingresar, enlistar, consultar, eliminar, modificar, salir, sesion, si, no As String
+    Public numero, nacimiento, antibi, preñez, estado, litros, grasa, urea, proteina, bacteria, somatica As String
+    Public gene, fecha_preñez, fecha_ida, fecha_vuelta, auxiliar As String
+    Public Sub Boton_idioma(btnOpcion As Button)
+        Select Case opcion
+            Case 0
+                btnOpcion.Text = ingresar
+            Case 1
+                btnOpcion.Text = modificar
+            Case 2
+                btnOpcion.Text = consultar
+            Case 3
+                btnOpcion.Text = eliminar
+        End Select
+    End Sub
+    Public Sub Variables_asignacion()
+        anestro = "anestro"
+        auxiliar = ""
+        Select Case idioma
+            Case 0
+                contraseña = "Contraseña"
+                usuario = "Usuario"
+                iniciar = "Iniciar"
+                ingresar = "Ingresar"
+                enlistar = "Enlistar"
+                consultar = "Consultar"
+                eliminar = "Emilinar"
+                modificar = "Modificar"
+                salir = "Salir"
+                sesion = "Cerrar sesion"
+                campo = "campo de recria"
+                rodeo = "rodeo de cria"
+                ninguno = "ninguno"
+                vaca = "vaca"
+                vaquillona = "vaquillona"
+                novillo = "novillo"
+                toro = "toro"
+                ternero = "ternero"
+                ternera = "ternera"
+                macho = "macho"
+                hembra = "hembra"
+                buscar = "buscar"
+                gen = "Genealogia"
+                ninguno = "Ninguno"
+                servicio = "Servicio"
+                preñada = "Preñada"
+                lactancia = "Lactancia"
+                seca = "Seca"
+                division = "Division"
+                lugar = "Lugar"
+                etapa = "Etapa"
+                sexo = "Sexo"
+                hembraa = "Hembra (antibiotico)"
+                numero = "Numero"
+                nacimiento = "nacimiento"
+                lugar = "lugar actual"
+                raza = "Raza"
+                division = "Division"
+                antibi = "antibiótico influyente"
+                preñez = "Taza de preñez"
+                preñada = "preñada"
+                grasa = "Grasa"
+                urea = "Urea"
+                bacteria = "recuento bacteriano"
+                somatica = "recuento de celulas somáticas" 'como agregar renglón
+                fecha_preñez = "fecha de preñez"
+                fecha_ida = "campo da recria (ida)"
+                fecha_vuelta = "campo da recria (vuelta)"
+                si = "Se"
+                no = "Não"
+            Case 1
+                contraseña = "Password"
+                usuario = "User"
+                iniciar = "Login"
+                ingresar = "Insert"
+                enlistar = "Show"
+                consultar = "Consult"
+                eliminar = "Remove"
+                modificar = "Modify"
+                salir = "Exit"
+                sesion = "Log out"
+                campo = "field of breeding"
+                rodeo = "breeding herd"
+                ninguno = "none"
+                vaca = "cow"
+                vaquillona = "heifer"
+                novillo = "steer"
+                toro = "bull"
+                ternero = "calf"
+                ternera = "veal"
+                macho = "male"
+                hembra = "female"
+                buscar = "search"
+                gen = "Genealogy"
+                ninguno = "none"
+                servicio = "Service"
+                preñada = "Pregnant"
+                lactancia = "Lactation"
+                seca = "Dry"
+                raza = "race"
+                division = "Division"
+                lugar = "Place"
+                etapa = "Stage"
+                hembraa = "female (antibiotic)"
+                numero = "Numer"
+                sexo = "Gender"
+                nacimiento = "Birth"
+                lugar = "Actual Place"
+                antibi = "Influential antibiotic"
+                preñez = "Pregnancy rate"
+                preñada = "Pregnant"
+                estado = "Stage"
+                litros = "Liters"
+                grasa = "Grease"
+                urea = "urea"
+                proteina = "Protein"
+                bacteria = "Bacterial count"
+                somatica = "Somatic cell count"
+                gen = "Genealogy"
+                fecha_preñez = "Pregnancy date"
+                fecha_ida = "breeding field (going)"
+                fecha_vuelta = "breeding field (coming)"
+                si = "yes"
+                no = "no"
+            Case 2
+                contraseña = "Senha"
+                usuario = "Usuário"
+                iniciar = "Começo"
+                ingresar = "Depósito"
+                enlistar = "Alistar"
+                eliminar = "Remover"
+                consultar = "Consultar"
+                modificar = "Mudança"
+                salir = "Sair"
+                sesion = "Perto"
+                campo = "campo da criaçâgo"
+                rodeo = "plantel"
+                ninguno = "Nenhum"
+                vaca = "vaca"
+                vaquillona = "novilha"
+                novillo = "novilho"
+                toro = "touro"
+                ternero = "vitela"
+                ternera = "vitela"
+                macho = "masculino"
+                hembra = "fêmea"
+                buscar = "pesquisa"
+                gen = "Genealogia"
+                ninguno = "Nenhum"
+                servicio = "Serviço"
+                preñada = "Gávida"
+                lactancia = "Lactação"
+                seca = "Seca"
+                raza = "Raça"
+                division = "Divisão"
+                lugar = "Localização"
+                etapa = "Etapa"
+                sexo = "Sexo"
+                hembraa = "Femenino (antibiotico)"
+                numero = "Numero"
+                nacimiento = "nascimento"
+                lugar = "localização actual"
+                division = "Divisão"
+                antibi = "antibiótico influente"
+                preñez = "Taxa de prenhez"
+                preñada = "Grávida"
+                grasa = "Graxa"
+                urea = "Uréia"
+                bacteria = "Contagem bacteriana"
+                somatica = "Contagem de células somáticas"
+                fecha_preñez = "data a graviez"
+                fecha_ida = "campo da criaçâgo (ida)"
+                fecha_vuelta = "campo da criaçâgo (retorno)"
+                si = "Se"
+                no = "Não"
+        End Select
+    End Sub
+    Function Validacion_numerica(ByVal valor As String, acum As Integer) As Integer
+        Dim retorno As Integer
+
+        If IsNumeric(valor) Then
+            If valor >= 0 Then
+                retorno = acum
+            Else
+                retorno = acum + 1
+            End If
+        Else
+            retorno = acum + 1
+        End If
+        Return retorno
+    End Function
+    Function Validacion_largo(ByVal tbxX As TextBox, acum As Integer) As Integer
+        Dim retorno As Integer
+        If IsNumeric(tbxX.Text()) Then
+            If tbxX.MaxLength() = tbxX.TextLength() Then
+                retorno = acum
+            Else
+                retorno = acum + 1
+            End If
+        Else
+            retorno = acum + 1
+        End If
+        Return retorno
+    End Function
+End Module
